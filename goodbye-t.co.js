b21472812d67207088d09015f98049d9946114fd
Xavier Ho <contact@xavierho.com>
2012-04-18 10:44:06 +1000

    Uses mouseover as well to change the links.

--- goodbye-t.co.js
+++ goodbye-t.co.js
@@ -1,7 +1,16 @@
-document.addEventLisener("mousedown", function (event) {
+document.addEventListener("mousedown", function (event) {
   var target = event.target;
   if (target.hasAttribute('data-ultimate-url'))
     target.href = target.getAttribute('data-ultimate-url');
   else if (target.hasAttribute('data-expanded-url'))
     target.href = target.getAttribute('data-expanded-url');
+}, true);
+
+document.addEventListener("mouseover", function (event) {
+  var target = event.target;
+  if (target.hasAttribute('data-ultimate-url')) {
+    target.title = target.href = target.getAttribute('data-ultimate-url');
+  } else if (target.hasAttribute('data-expanded-url')) {
+    target.title = target.href = target.getAttribute('data-expanded-url');
+  }
 }, true);
