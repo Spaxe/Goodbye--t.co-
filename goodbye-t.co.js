546e759d244036bfab9ef2abac29c82d200d4ed6
Xavier Ho <contact@xavierho.com>
2014-02-25 22:20:07 +1100

    Version 1.4 final

--- goodbye-t.co.js
+++ goodbye-t.co.js
@@ -7,10 +7,20 @@ function findAnchor (node) {
 function replaceHref (target) {
   if (target.nodeName != "A")
     return;
-  if (target.hasAttribute('data-expanded-url'))  // For Twitter
-    target.title = target.href = target.getAttribute('data-expanded-url');
-  else if (target.hasAttribute('data-full-url')) // For Tweetdeck
-    target.title = target.href = target.getAttribute('data-full-url');
+
+  // Twitter
+  if (target.hasAttribute('data-expanded-url')) {
+    target.href = target.getAttribute('data-expanded-url');
+    return;
+  } else if (target.hasAttribute('data-pre-embedded') && target.getAttribute('data-pre-embedded') == "true") {
+    target.href = window.location.protocol + "//" + target.text;
+    return;
+  }
+
+  // Tweetdeck
+  if (target.hasAttribute('data-full-url')) {
+    target.href = target.getAttribute('data-full-url');
+  }
 }
 
 function bypass_t_co (event) {
