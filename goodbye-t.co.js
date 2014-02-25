4738b6dd7e490c9bf231338df542ffd73a115222
Xavier Ho <contact@xavierho.com>
2014-02-25 21:18:41 +1100

    Updating to version 1.4, supporting Tweetdeck.

--- goodbye-t.co.js
+++ goodbye-t.co.js
@@ -7,20 +7,18 @@ function findAnchor (node) {
 function replaceHref (target) {
   if (target.nodeName != "A")
     return;
-  if (target.hasAttribute('data-expanded-url'))
+  if (target.hasAttribute('data-expanded-url'))  // For Twitter
     target.title = target.href = target.getAttribute('data-expanded-url');
+  else if (target.hasAttribute('data-full-url')) // For Tweetdeck
+    target.title = target.href = target.getAttribute('data-full-url')
 }
 
-document.addEventListener("mousedown", function (event) {
+function bypass_t_co(event) {
   try {
     var target = findAnchor(event.target);
     replaceHref(target);
   } catch (e) {}
-}, true);
+}
 
-document.addEventListener("mouseover", function (event) {
-  try {
-    var target = findAnchor(event.target);
-    replaceHref(target);
-  } catch (e) {}
-}, true);
+document.addEventListener("mousedown", bypass_t_co, true);
+document.addEventListener("mouseover", bypass_t_co, true);
